d60e5b38ee66ed3ef52d1f224adfff26b589a9cf
CarlosBeltran <carlosbp2508@gmail.com>
2023-08-29 14:37:49 -0500

    se actulizo el print

--- hellogit.java
+++ hellogit.java
@@ -1,5 +1,6 @@
 public class hellogit{
     public static void main (String[] args){
         System.out.println("Hola Git");
+        System.out.println("modificando el segundo archivo");
     }
 }
