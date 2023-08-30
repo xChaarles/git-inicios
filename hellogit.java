3fb2e51d4d636e421330e84810e4efbffac01a07
CarlosBeltran <carlosbp2508@gmail.com>
2023-08-29 22:38:37 -0500

    ultima modificacion de commit

--- hellogit.java
+++ hellogit.java
@@ -1,6 +1,7 @@
 public class hellogit{
     public static void main (String[] args){
         System.out.println("Hola Git");
-        System.out.println("modificando el segundo archivo");
+        System.out.println("modificando el segundo archivo en el commit");
+        System.out.println("vamos a ver el comando diff");
     }
 }
